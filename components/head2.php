488d4c8d8db6cd38362c8c5b4af8d139d4b7273f
adens <asepindrakurniawan@gmail.com>
2023-04-05 06:30:54 +0700

    fixing user picture not showing

--- components/head2.php
+++ components/head2.php
@@ -248,7 +248,7 @@
           <li class="nav-item dropdown pe-3">
 
             <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
-              <img src="<?=$user->image?>" alt="Profile" class="rounded-circle">
+              <img src="<?=$image?><?=$user->image?>" alt="Profile" class="rounded-circle">
               <span class="d-none d-md-block dropdown-toggle ps-2"><?=$user->username?></span>
             </a><!-- End Profile Iamge Icon -->
 
