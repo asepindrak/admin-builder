bba5ba207772c68df6bf85387211934729ee3488
adens <asepindrakurniawan@gmail.com>
2023-04-05 06:29:45 +0700

    show header

--- components/head2.php
+++ components/head2.php
@@ -78,7 +78,7 @@
   <body>
 
     <!-- ======= Header ======= -->
-    <header id="header" class="header fixed-top d-flex align-items-center hidden">
+    <header id="header" class="header fixed-top d-flex align-items-center">
 
       <div class="d-flex align-items-center justify-content-between">
         <a href="index.html" class="logo d-flex align-items-center">
